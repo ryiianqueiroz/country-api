68d6065e074d18045cc3b45ca75d590455746143
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-05-28 22:37:37 -0300

    working in json

--- src/components/CountryPage/CountryPage.jsx
+++ src/components/CountryPage/CountryPage.jsx
@@ -12,11 +12,8 @@ function CountryPage() {
   let location = useLocation();
   let ccn3 = location.pathname.replaceAll("/", "")
 
-  let { nome, flag, population, region, sub_region, capital, top_level_domain } = ""
-  let { native_name, nativo } = ""
-  let { currencies, currencies_sub } = ""
-  let { languages, languages_sub } = ""
-
+  let { nome, flag, currencies, currencies_sub, languages, languages_sub } = ""
+  let top_level_domain = []
 
   //  useEffect(() => {
   //    async function fetchApi(code) {
@@ -48,66 +45,52 @@ function CountryPage() {
 
   useEffect(() => {
     async function fetchApi(code) {
-      const response = await fetch("/data.json");
-      const data = await response.json();
-
-      const indexPosition = ( data ) => { if ( data.numericCode == code ) { return true } }
-
-      const index = data.findIndex( indexPosition )
-      setApi(data[index])
-      
-    
-      data.map((key) => {
-          if (Array.isArray(key[index].borders)) {
-            const borderPromises = key[index].borders.map( async ( borderCode ) => {
-              const borderResponse = await fetch("/data.json");
-              const borderData = await borderResponse.json();
-
-              borderData.map((key) => {
-                return key[index].name.common;
-              })
-            });
-  
-            const bordersNames = async () => { return await Promise.all(borderPromises) }
-            setBorders(bordersNames);
-          } else {
-            setBorders([]);
-          }
+      fetch('/data.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-      )
-    } 
+        return response.json();
+      })
+      .then(data => {
+        const indexPosition = ( dados ) => { if ( dados.numericCode == code ) { return true } }
+
+        const index = data.findIndex( indexPosition )
+        setApi([data[index]])
+      })
+      .catch(error => console.error('Error fetching countries:', error));
+    }
+
+    // async function fetchBorders(code) {
+    //   const response = await fetch("/data.json");
+    //   const data = await response.json();     
+    // } 
 
     fetchApi(ccn3)
+    //fetchBorders(ccn3)
   }, [ccn3])
 
   console.log(api)
 
-  api.map((post) => {
-      nome = post.name.common
-      population = post.population
-      region = post.region
-      sub_region = post.subregion
-      capital = post.capital
-      top_level_domain = post.tld[0]
-      flag = post.flags.png
+  api.map(post => {
+    console.log("sim")
+    nome = post.name
+    flag = post.flags.png
 
+    Object.keys(post.currencies).forEach(function (prop) {
+      currencies_sub = prop
+    });
 
-      Object.keys(post.name.nativeName).forEach(function (prop) {
-        nativo = prop
-      });
+    Object.keys(post.languages).forEach(function (prop) {
+      languages_sub = prop
+    });
 
-      Object.keys(post.currencies).forEach(function (prop) {
-        currencies_sub = prop
-      });
+    top_level_domain = post.topLevelDomain
+    currencies = post.currencies[`${currencies_sub}`].symbol
+    languages = post.languages[`${languages_sub}`].name
 
-      Object.keys(post.languages).forEach(function (prop) {
-        languages_sub = prop
-      });
-
-      native_name = post.name.nativeName[`${nativo}`].common
-      currencies = post.currencies[`${currencies_sub}`].symbol
-      languages = post.languages[`${languages_sub}`]
-    })
+    console.log(top_level_domain, currencies, languages)
+  })
 
   return (
     <div className={`${darkMode ? "bg-[#202c37]" : ""} min-h-[100vh] py-[100px]`}>
@@ -121,9 +104,9 @@ function CountryPage() {
         <div className="w-[44%] flex flex-col justify-center lg:w-[49%]">
           <h1 className={`${darkMode ? "text-white" : ""} text-[1.3rem] font-bold lg:text-[1rem]`}>{nome}</h1>
 
-          <div className="grid grid-cols-2 mt-2">
+          {/* <div className="grid grid-cols-2 mt-2">
             <div className="flex flex-col gap-1">
-              <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Native Name: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{native_name}</span></h1>
+              <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Native Name: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{nome}</span></h1>
               <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Population: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{population}</span></h1>
               <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Region: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{region}</span></h1>
               <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Sub Region: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{sub_region}</span></h1>
@@ -135,9 +118,27 @@ function CountryPage() {
               <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Currencies: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{currencies}</span></h1>
               <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Languages: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{languages}</span></h1>
             </div>
-          </div>
+          </div> */}
 
-          {borders.length > 0 ? (
+          {api.map((post) => (
+            <div key={post.numericCode} className="grid grid-cols-2 mt-2">
+              <div className="flex flex-col gap-1">
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Native Name: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{post.name}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Population: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{post.population}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Region: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{post.region}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Sub Region: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{post.sub_region}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Capital: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{post.capital}</span></h1>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Top Level Domain: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{top_level_domain}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Currencies: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{currencies}</span></h1>
+                <h1 className={`${darkMode ? "text-[#dadada]" : ""} text-[0.9rem] lg:text-[0.7rem] font-bold`}>Languages: <span className={`${darkMode ? "text-[#dadadab9]" : ""} text-[0.8rem] lg:text-[0.7rem] font-normal`}>{languages}</span></h1>
+              </div>
+            </div>
+          ))}
+
+          {/* {borders.length > 0 ? (
             <div className={`${darkMode ? "text-white" : ""} mt-4 flex`}>
               <div className="grid grid-cols-3 gap-x-2 gap-y-3 items-center">
                 <span className="mr-3 text-[0.9rem] font-bold lg:text-[0.7rem] lg:text-nowrap">Borders Countries: </span>              
@@ -146,7 +147,7 @@ function CountryPage() {
               ))}
               </div>
             </div>
-          ) : (<p></p>)}
+          ) : (<p></p>)} */}
         </div>
       </div>
     </div>
